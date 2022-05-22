08e951115f78362dbc3b8df554d09a3ed3e587ce
Crowdbotics <team@crowdbotics.com>
2022-05-22 11:21:49 +0000

    Update models

--- backend/assault_rape_app_35373/wsgi.py
+++ backend/assault_rape_app_35373/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assault_rape_app_35373.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "assault_rape_app_35373.settings")
 
 application = get_wsgi_application()
