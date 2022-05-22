98d8796ae91dfca849e12a4a2e780a34c501ec40
Crowdbotics <team@crowdbotics.com>
2022-05-22 11:15:02 +0000

    Initial commit.

---
+++ backend/assault_rape_app_35373/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for assault_rape_app_35373 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assault_rape_app_35373.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "assault_rape_app_35373",
+  url: "https://assault_rape_app_35373.botics.co",
+  api: "https://assault_rape_app_35373.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
